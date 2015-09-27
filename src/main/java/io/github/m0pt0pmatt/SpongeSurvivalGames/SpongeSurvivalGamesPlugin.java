feff22255af342baf807be66c774b9dd57b999f1
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2015-09-27 14:38:41 -0600

    Injected Logger to Main Class

--- src/main/java/io/github/m0pt0pmatt/SpongeSurvivalGames/SpongeSurvivalGamesPlugin.java
+++ src/main/java/io/github/m0pt0pmatt/SpongeSurvivalGames/SpongeSurvivalGamesPlugin.java
@@ -3,8 +3,27 @@ package io.github.m0pt0pmatt.SpongeSurvivalGames;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
 
 @Plugin(id = "spongesurvivalgames", name = "Sponge Survival Games", version = "1.0")
 public class SpongeSurvivalGamesPlugin {
+
+    // Plugin Logger
+    private Logger logger;
+
+    @Inject
+    public SpongeSurvivalGamesPlugin(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        logger.info("Sponge Survival Games Plugin Enabled");
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
 
 }
