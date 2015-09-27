e78195fdb5d64783cf9df270ecf14e9a45278587
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2015-09-27 14:20:59 -0600

    Initial commit.

---
+++ src/main/java/io/github/m0pt0pmatt/SpongeSurvivalGames/SpongeSurvivalGamesPlugin.java
@@ -0,0 +1,10 @@
+package io.github.m0pt0pmatt.SpongeSurvivalGames;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "spongesurvivalgames", name = "Sponge Survival Games", version = "1.0")
+public class SpongeSurvivalGamesPlugin {
+
+}
