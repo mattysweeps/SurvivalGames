e69351612e0cd38a936784acc3936e4855c269f8
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2015-10-04 22:17:45 -0600

    player contained in glass at start, 10 second countdown (bugged but working), player in adventure mode

--- src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/SurvivalGame.java
+++ src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/SurvivalGame.java
@@ -28,7 +28,15 @@ package io.github.m0pt0pmatt.spongesurvivalgames;
 import java.util.Optional;
 
 import io.github.m0pt0pmatt.spongesurvivalgames.exceptions.*;
+import io.github.m0pt0pmatt.spongesurvivalgames.tasks.CountdownTask;
+import org.spongepowered.api.block.*;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.entity.GameModeData;
+import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameMode;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
@@ -36,6 +44,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * represents a Survival Game.
@@ -50,7 +59,6 @@ public class SurvivalGame {
     private Optional<Location<World>> centerLocation = Optional.empty();
     private final Set<UUID> playerSet = new HashSet<>();
     private int playerLimit = 25; //Default player limit
-
 
     public SurvivalGame(SpongeSurvivalGamesPlugin plugin) {
         this.plugin = plugin;
@@ -99,6 +107,7 @@ public class SurvivalGame {
         //Spawn players
 
         Set<UUID> missingPlayers = new HashSet<>();
+        Set<Player> players = new HashSet<>();
         Set<Location<World>> spawnLocations = new HashSet<>();
         spawnLocations.addAll(this.spawnLocations);
 
@@ -108,20 +117,119 @@ public class SurvivalGame {
             if (!player.isPresent()) {
                 missingPlayers.add(playerUUID);
             } else {
-                Location<World> spawnPoint = spawnIterator.next();
-
-                //Teleport player
-                player.get().setLocation(spawnPoint);
-
-                //Postion player to look at center
-                if (centerLocation.isPresent()) {
-                    player.get().setRotation(player.get().getRotation().add(centerLocation.get().getPosition())); //TODO: compute real rotation
-                }
+                players.add(player.get());
             }
 
         }
 
         playerSet.removeAll(missingPlayers);
+
+        for (Player player: players) {
+
+            Location<World> spawnPoint = spawnIterator.next();
+
+            //Teleport player
+            player.setLocation(spawnPoint.add(0.5, 0, 0.5));
+
+            //Postion player to look at center
+            if (centerLocation.isPresent()) {
+                player.setRotation(player.getRotation().add(centerLocation.get().getPosition())); //TODO: compute real rotation
+            }
+
+            player.offer(Keys.GAME_MODE, GameModes.ADVENTURE);
+
+
+        }
+
+        for (Location<World> location: spawnLocations){
+            final Set<BlockSnapshot> snapshots = new HashSet<>();
+            snapshots.add(location.add(1, 0, 0).createSnapshot());
+            snapshots.add(location.add(1, 1, 0).createSnapshot());
+            snapshots.add(location.add(-1, 0, 0).createSnapshot());
+            snapshots.add(location.add(-1, 1, 0).createSnapshot());
+            snapshots.add(location.add(0, 0, 1).createSnapshot());
+            snapshots.add(location.add(0, 1, 1).createSnapshot());
+            snapshots.add(location.add(0, 0, -1).createSnapshot());
+            snapshots.add(location.add(0, 1, -1).createSnapshot());
+            snapshots.add(location.add(0, 2, 0).createSnapshot());
+            snapshots.add(location.add(0, -1, 0).createSnapshot());
+
+            location.add(1, 0, 0).setBlockType(BlockTypes.GLASS);
+            location.add(1, 1, 0).setBlockType(BlockTypes.GLASS);
+            location.add(-1, 0, 0).setBlockType(BlockTypes.GLASS);
+            location.add(-1, 1, 0).setBlockType(BlockTypes.GLASS);
+            location.add(0, 0, 1).setBlockType(BlockTypes.GLASS);
+            location.add(0, 1, 1).setBlockType(BlockTypes.GLASS);
+            location.add(0, 0, -1).setBlockType(BlockTypes.GLASS);
+            location.add(0, 1, -1).setBlockType(BlockTypes.GLASS);
+            location.add(0, 2, 0).setBlockType(BlockTypes.GLASS);
+            location.add(0, -1, 0).setBlockType(BlockTypes.GLASS);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(0, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(10, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(1, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(9, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(2, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(8, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(3, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(7, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(4, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(6, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(5, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(5, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(6, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(4, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(7, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(3, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(8, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(2, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(9, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(1, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(10, TimeUnit.SECONDS)
+                    .execute(new CountdownTask(0, players))
+                    .submit(plugin);
+
+            plugin.getGame().getScheduler().createTaskBuilder()
+                    .delay(10, TimeUnit.SECONDS)
+                    .execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            for (BlockSnapshot snapshot : snapshots) snapshot.restore(true, false);
+                        }
+                    })
+                    .submit(plugin);
+        }
 
     }
 
