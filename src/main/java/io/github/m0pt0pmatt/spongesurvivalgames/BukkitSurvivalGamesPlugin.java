cb8ed417c9b509f92a805a04206d148794427207
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2015-11-02 06:37:19 -0700

    started working on commands

--- src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/BukkitSurvivalGamesPlugin.java
+++ src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/BukkitSurvivalGamesPlugin.java
@@ -38,6 +38,8 @@ import io.github.m0pt0pmatt.spongesurvivalgames.commands.game.running.ForceStopG
 import io.github.m0pt0pmatt.spongesurvivalgames.commands.game.stopped.*;
 import io.github.m0pt0pmatt.spongesurvivalgames.events.PlayerDeathEventListener;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -75,6 +77,139 @@ public class BukkitSurvivalGamesPlugin  extends JavaPlugin {
         survivalGameMap.clear();
         Bukkit.getServer().getPluginManager().registerEvents(new PlayerDeathEventListener(), this);
         Bukkit.getLogger().info("Sponge Survival Games Plugin Enabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args){
+
+        Map<String, String> arguments = new HashMap<>();
+
+        //ssg create [id]
+        //ssg list
+        //ssg delete [id]
+        //ssg print center [id]
+        //ssg print chest midpoint [id]
+        //ssg print chest range [id]
+        //ssg print countdown [id]
+        //ssg print exit [id]
+        //ssg print player-limit [id]
+        //ssg print spawns [id]
+        //ssg print world [id]
+        //ssg add spawn [id] [x] [y] [z]
+        //ssg clear spawns [id]
+        //ssg set center [id] [x] [y] [z]
+        //ssg set chest midpoint [id] [midpoint]
+        //ssg set chest range [id] [range]
+        //ssg set countdown [id] [countdown]
+        //ssg set exit [id] [worldname] [x] [y] [z]
+        //ssg set player-limit [id] [player-limit]
+        //ssg set world [id] [worldname]
+        //ssg ready [id]
+        //ssg add player [id]
+        //ssg remove player [id]
+        //ssg start [id]
+        //ssg stop [id]
+        //ssg force-stop [id]
+        //ssg load [id] [filename]
+        //ssg save [id] [filename]
+
+        if (!cmd.getName().equalsIgnoreCase("ssg")){
+            return true;
+        }
+        if (args.length < 1){
+            return false;
+        }
+
+        if (args[0].equalsIgnoreCase("create")){
+            //ssg create [id]
+            if (args.length == 1) return false;
+            if (args.length > 2) return false;
+            arguments.put("id", args[1]);
+            return new CreateGameCommand(arguments).execute(sender);
+        }
+        else if (args[0].equalsIgnoreCase("list")){
+            //ssg list
+            if (args.length > 1) return false;
+            return new ListGamesCommand(arguments).execute(sender);
+        }
+        else if (args[0].equalsIgnoreCase("delete")){
+            //ssg delete [id]
+            if (args.length == 1) return false;
+            if (args.length > 2) return false;
+            arguments.put("id", args[1]);
+            return new DeleteGameCommand(arguments).execute(sender);
+        }
+        else if (args[0].equalsIgnoreCase("print")){
+
+            if (args.length < 3) return false;
+
+            arguments.put("id", args[2]);
+
+            if (args[1].equalsIgnoreCase("center")){
+                //ssg print center [id]
+            }
+            else if (args[1].equalsIgnoreCase("chest")){
+                //ssg print chest midpoint [id]
+                //ssg print chest range [id]
+            }
+            else if (args[1].equalsIgnoreCase("countdown")){
+                //ssg print countdown [id]
+            }
+            else if (args[1].equalsIgnoreCase("exit")){
+                //ssg print exit [id]
+            }
+            else if (args[1].equalsIgnoreCase("player-limit")){
+                //ssg print player-limit [id]
+            }
+            else if (args[1].equalsIgnoreCase("spawns")){
+                //ssg print spawns [id]
+            }
+            else if (args[1].equalsIgnoreCase("world")){
+                //ssg print world [id]
+            }
+            else{
+                return false;
+            }
+        }
+        else if (args[0].equalsIgnoreCase("add")){
+            //ssg add spawn [id] [x] [y] [z]
+            //ssg add player [id]
+        }
+        else if (args[0].equalsIgnoreCase("clear")){
+            //ssg clear spawns [id]
+        }
+        else if (args[0].equalsIgnoreCase("set")){
+            //ssg set center [id] [x] [y] [z]
+            //ssg set chest midpoint [id] [midpoint]
+            //ssg set chest range [id] [range]
+            //ssg set countdown [id] [countdown]
+            //ssg set exit [id] [worldname] [x] [y] [z]
+            //ssg set player-limit [id] [player-limit]
+            //ssg set world [id] [worldname]
+        }
+        else if (args[0].equalsIgnoreCase("remove")){
+            //ssg remove player [id]
+        }
+        else if (args[0].equalsIgnoreCase("ready")){
+            //ssg ready [id]
+        }
+        else if (args[0].equalsIgnoreCase("start")){
+            //ssg start [id]
+        }
+        else if (args[0].equalsIgnoreCase("stop")){
+            //ssg stop [id]
+        }
+        else if (args[0].equalsIgnoreCase("force-stop")){
+            //ssg force-stop [id]
+        }
+        else if (args[0].equalsIgnoreCase("load")){
+            //ssg load [id] [filename]
+        }
+        else if (args[0].equalsIgnoreCase("save")){
+            //ssg save [id] [filename]
+        }
+
+        return false;
     }
 
     private void registerCommands() {
