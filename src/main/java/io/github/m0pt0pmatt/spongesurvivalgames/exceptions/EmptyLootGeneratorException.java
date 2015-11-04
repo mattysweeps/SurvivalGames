9543de174162c3222f77b4e2c37ddc084c999bb0
Skyler Manzanares <skymanzanares@hotmail.com>
2015-11-03 21:37:53 -0700

    Created loot system

---
+++ src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/exceptions/EmptyLootGeneratorException.java
@@ -0,0 +1,5 @@
+package io.github.m0pt0pmatt.spongesurvivalgames.exceptions;
+
+public class EmptyLootGeneratorException extends SurvivalGameException {
+
+}
---
+++ src/main/java/io/github/m0pt0pmatt/spongesurvivalgames/loot/Loot.java
@@ -0,0 +1,30 @@
+package io.github.m0pt0pmatt.spongesurvivalgames.loot;
+
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Wrapper class for Loot which houses information about it's weight and the item it's storing
+ * @author Skyler
+ *
+ */
+public class Loot {
+
+	private ItemStack item;
+	
+	private double weight;
+	
+	public Loot(ItemStack item, double weight) {
+		this.item = item;
+		this.weight = weight;
+	}
+
+	public ItemStack getItem() {
+		return item;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+	
+	
+}
